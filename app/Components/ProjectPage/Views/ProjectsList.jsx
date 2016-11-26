a7867722fcd8fb6c0afc61eb32f4ca77cf214683
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-11-26 11:45:17 -0200

    Added list of items

--- app/Components/ProjectPage/Views/ProjectsList.jsx
+++ app/Components/ProjectPage/Views/ProjectsList.jsx
@@ -13,9 +13,22 @@ export default class ProjectList extends React.Component {
   }
 
   render(): ?React$Element<div> {
+    let projectsList = this.props.projects
+      .map((project: objects): ?React$Element<div> => {
+        return(
+          <div key={project.id} className="container">
+            {project.full_name}
+          </div>
+        );
+      });
+
     return(
       <div className="column is-8">
         ProjectList
+        <br />
+        <div className="container">
+          {projectsList}
+        </div>
       </div>
     );
   }
